a198af0536fc3467dfb89e939d3710368711b020
Charlotte Thomas <contact@nwa2coco.fr>
2022-03-13 11:57:47 +0100

    first parsing (parsing line and generalities)

--- lib/lib.ml
+++ lib/lib.ml
@@ -1,14 +1,15 @@
-type chapter = Nil | Chap of string * string * string list (*name # list of lines*)
+type chapter = Nil | Chap of string * string * string list * int;; (*name # list of lines*)
+
 type book = chapter list
 let string_to_list s = List.of_seq (String.to_seq s);;
 let parse_book ?(book=true) str = 
-    let a = if book then Str.regexp "\\chapter" else Str.regexp "\\section" in
+    let a = if book then Str.regexp "\\\\chapter" else Str.regexp "\\\\section" in
     Str.split a str;;
 
 
 let print_chapter = function
   | Nil -> print_string "Nil\n"
-  | Chap(s,_,q) -> Printf.printf "Name : %s\n" s; List.iteri (fun i str -> Printf.printf "Line %d : %s\n" i str) q
+  | Chap(s,_,q,_) -> Printf.printf "Name : %s\n" s; List.iteri (fun i str -> Printf.printf "Line %d : %s\n" i str) q
 
 let read_file filename = 
   let lines = ref [] in
@@ -39,7 +40,7 @@ let transform_list_of_chars_to_string_without_the_newline lst =
   in aux "" lst
 
   
-let extract_chapter chapter = 
+let extract_chapter chapter i = 
   let rec extract_name lst acc = 
     match lst with 
       | [] -> acc,""
@@ -47,18 +48,63 @@ let extract_chapter chapter =
       | t::q when t='{' -> extract_name q acc
       | t::q when t='\\' -> extract_name q acc
       | t::q -> extract_name q (acc^(String.make 1 t)) in
-  let line_list = List.filter (fun a -> not (String.equal a "\\")) (Str.split (Str.regexp "\n") chapter) in
+  let line_list = List.filter (fun a -> let a = String.trim a in not (String.equal a "\\") && not (String.equal a "") && not (String.equal a "\n")) 
+                (Str.split (Str.regexp "\n") chapter) in
   match line_list with 
     | [] -> Nil
     | t::q -> let name,r = extract_name (string_to_list t) "" in 
     let q = if r="" then q else r::q in
     let total_str = String.concat "\n" q in
-    Chap(name,total_str,q)
+    Chap(name,total_str,q,i)
 
 let extract_chapters file =
   let str = read_file file in
   let chappies = parse_book str in
+  let a = ref (-1) in
   let rec aux acc = function
     | [] -> acc
-    | t::q -> aux ((extract_chapter t)::acc) q
-  in (aux [] chappies : book);;
+    | t::q -> incr a; aux ((extract_chapter t !a)::acc) q
+  in List.rev(aux [] chappies : book);;
+
+  let replace_generalities str = 
+    let newline_regexp = Str.regexp "\\\\newline" 
+    and par_regexp = Str.regexp "\\\\par"
+    and bigskip_regexp = Str.regexp "\\\\bigskip"
+    and doublebackslash_regexp = Str.regexp "\\\\\\\\"
+    and sep_regexp = Str.regexp "\\\\sep"
+    and begin_center_regexp = Str.regexp "\\\\begin{center}"
+    and end_center_regexp = Str.regexp "\\\\end{center}"
+    and end_document_regexp = Str.regexp "\\\\end{document}" 
+  in 
+    Str.global_replace newline_regexp "<br/>\n" @@
+    Str.global_replace par_regexp "<br/>\n" @@
+    Str.global_replace bigskip_regexp "</p>\n\n<p>\n" @@
+    Str.global_replace doublebackslash_regexp "<br>\n" @@
+    Str.global_replace sep_regexp "<br/><div class=\"center\">\n<p>\n<br/><b>***</b><br/>\n</p>\n</div>" @@
+    Str.global_replace begin_center_regexp "<div class=\"center\">\n" @@
+    Str.global_replace end_center_regexp "\n</div>" @@
+    Str.global_replace end_document_regexp "" @@
+    str
+
+
+let transform_chapter chapter = 
+  match chapter with
+    | Nil -> Nil
+    | Chap(s,_,q,i) -> let g = List.map replace_generalities q in 
+                              let c = String.concat "\n" g in 
+                              let c = "<div>\n\t" ^ (String.trim c) ^ "\n<div><br/>\n" in
+                              Chap(String.trim s,c,g,i);;
+
+
+  let chapter_to_string chapter = 
+    match chapter with 
+      | Nil -> ""
+      | Chap(s,c,_,i) -> let line = "<h2 id=\"" ^ (string_of_int i) ^ "\"> Chapter " ^ (string_of_int i) ^ " : " ^ s ^ "</h2>\n<br>" in line ^ c;;
+
+  let write_to_file file str =
+    let f = open_out file in
+    output_string f str;
+    flush f;
+    close_out f;;
+
+  let test_chapter_to_file chapter file = write_to_file file (chapter_to_string chapter);;
--- test/htmlfromtexbooks.ml
+++ test/htmlfromtexbooks.ml
@@ -1,4 +1,5 @@
 open Htmlfromtexbooks.Lib
 let s = read_file "/home/charlotte/Documents/dev/htmlfromtexbooks/test.txt";;
 let l = parse_book s;;
-let _ = List.iter (fun c -> print_chapter (extract_chapter c)) l
+let chapters = List.mapi (fun i a -> transform_chapter (extract_chapter a i)) l;;
+let _ = List.iteri (fun i a -> test_chapter_to_file a ("test"^(string_of_int i)^".html" )) chapters;;
