98cca62b710c3ed06786f07295c4e1e63a8403a3
Charlotte Thomas <contact@nwa2coco.fr>
2022-03-19 14:51:04 +0100

    starting new parser

--- lib/parser.ml
+++ lib/parser.ml
@@ -1,193 +1,84 @@
-(**Type representing a section*)
-type section = Nil (**Nul*)
-             | Node of string * string list  (**Node: a node is a string, the text of the node, text list *)
-             | BigNode of section list
-             | Subsubsection of string * section list * int
-             | Subsection of string * section list * int (**Subsection: a subsection is the subsection of a text*)
-             | Section of string * section list * int (**A section is a name and the list of all nodes/subsection*)
-             | Chap of string * section  list * int
-
-(** Glossary type : It can be a Nil or an "Entry" : a tuple of a string (the key), a string (the name) and a string (the definition) *)
-type glossary = Nul | Entry of string * string * string;;
-
-
-let put_int_into_node i = function
-  | Nil -> Nil
-  | Node(a,b) -> Node(a,b)
-  | Subsubsection(a,b,_) -> Subsubsection(a,b,i)
-  | Subsection(a,b,_) -> Subsection(a,b,i)
-  | Section(a,b,_) -> Section(a,b,i)
-  | BigNode(a) -> BigNode(a)
-  | Chap(a,b,_) -> Chap(a,b,i);;
-
-let rec expand a lst = 
-  match a with
-  | [] -> lst
-  | t::q -> expand q (t::lst);;
-
-let shuffle lst = 
-  let rec redistribute i acc lst = match lst with
-    | [] -> acc
-    | Nil::q -> redistribute i acc q
-    | Node(a,b)::q -> redistribute i (Node(a,b)::acc) q
-    | Subsubsection(a,b,_)::q -> redistribute (i+1) (Subsubsection(a,b,i)::acc) q
-    | Subsection(a,b,_)::q -> redistribute (i+1) (Subsection(a,b,i)::acc) q
-    | Section(a,b,_)::q -> redistribute (i+1) (Section(a,b,i)::acc) q
-    | Chap(a,b,_)::q -> redistribute (i+1) (Chap(a,b,i)::acc) q
-    | BigNode(a)::q -> redistribute (i) (expand (a) acc) q
-  in List.rev (redistribute 0 [] lst);;
+type cmd = 
+  | NullCommand
+  | AtomCmd of string
+  | SimpleCmd of string * string
+  | MultipleCmd of string * string list
+type structure = 
+  | Nul
+  | Line of string
+  | Cmd of cmd 
+  | Env of string * structure list
+  | Subsubsection of string * structure
+  | Subsection of string * structure
+  | Section of string * structure
+  | Chapter of string * structure
+  | Document of structure
+  | Preamble of structure
+type ast = 
+  | Nil 
+  | Node of structure * ast * ast
 
 
-let print_list_of_section ?(start_chapter=1) ?(specific_chapter=(-1)) section =
-  let rec aux result lst = match lst with
-    | [] -> result
-    | Nil::q -> aux result q
-    | Node(s,_)::l -> aux (result^Printf.sprintf "%s\n" s) l
-    | BigNode(a)::q -> let r = aux "" a in  aux (result^r) q
-    | Subsubsection(s,sec,i)::l -> 
-      let results = aux "" sec in 
-      let title = Printf.sprintf "<h4 id=\"%d\">Subsubsection %d : %s</h4><br>\n" (i+1_000_000_000) i (String.trim(s)) in
-      let total = result^title^results in
-      aux total l
-    | Subsection(s,sec,i)::l ->
-      let results = aux "" sec in 
-      let title = Printf.sprintf "<h3 id=\"%d\">Subsection %d : %s</h3><br>\n" (i+1_000_000) i (String.trim((s))) in
-      let total = result^title^results in
-      aux total l
-    | Section(s,sec,i)::l ->
-      let results = aux "" sec in
-      let title = Printf.sprintf "<h3 id=\"%d\">Section %d : %s</h3><br>\n" (i+1000) i (String.trim(s)) in
-      let total = result^title^results in
-      aux total l
-    | Chap(s,sec,i)::l when i=specific_chapter ->
-      let results = aux "" sec in 
-      let title = Printf.sprintf "<h1 id=\"%d\">Chapter %d : %s<h1><br>\n" i i (String.trim(s)) in 
-      let total = result^title^results in
-      aux total l
-    | Chap(s,sec,i)::l when i>=start_chapter && i=(-1)->
-      let results = aux "" sec in 
-      let title = Printf.sprintf "<h1 id=\"%d\">Chapter %d : %s </h1><br>\n"  i i (String.trim(s)) in
-      let total = result^title^results in
-      aux total l
-    | _::l -> aux result l
-  in aux "" section;;
+(**
+Si on rencontre un \
+  => On appelle parse_command
+*)
+
+(**
+Parse_command lit les chars 
+  => si il rencontre un { sur une AtomCmd
+    => SimpleCmd (String text) (...)
+  => Si il rencontre un { sur une SimpleCmd
+    => Création d'une liste
+  => Si il rencontre un { sur une MultipleCmd ajoute à la commande
+  Quand on rencontre le dernier } => on renvoie
+*)
 
 
-let print_table_of_content ?(start_chapter=1) section = 
-  let rec aux result lst = match lst with
-    | [] -> result
-    | Nil::q -> aux result q
-    | Node(_,_)::q -> aux result q
-    | Subsubsection(name,_,i)::l ->
-      let title = Printf.sprintf "<li><a href=\"#%d\"><b>Subsubsection %d : %s</b></a></li>\n" (i+1_000_000_000) (i) name in
-      aux (result^title) l
-    | Subsection(name,list_of_subsubsection,i)::l -> 
-      let title = Printf.sprintf "<li><a href=\"#%d\"><b>Subsection %d : %s<b></a></li>\n<ul>" (i+1_000_000) (i) name  in 
-      let title = title ^ (aux "" list_of_subsubsection) in
-      let title = title ^ "</ul>\n"
-      in aux (result^title) l
-    | Section(name,list_of_sections,i)::l -> 
-      let title = Printf.sprintf "<li><a href=\"#%d\"><b>Section %d : %s</b></a></li>\n<ul>" (i+1000) (i) name in
-      let title = title ^ (aux "" list_of_sections) in
-      let title = title ^ "</ul>\n"
-      in aux (result^title) l
-    | Chap(name,list_of_sections,i)::l when i>=start_chapter ->
-      let title = Printf.sprintf "<li><a href=\"#%d\"><b>Chapter %d : %s</h2></b></a></li>\n<ul>" (i) (i) name in
-      let title = title ^ (aux "" list_of_sections) in
-      let title = title ^ "</ul>\n" in
-      aux (result^title) l 
-    | _::q -> aux result q
-  in (aux "<div class=\"center\"><ul><br>\n" section)^("</ul></div>\n")
+let rec parse_interior_of_an_accolade list_of_chars acc = 
+  match list_of_chars with 
+    | [] -> acc,[]
+    | t::q when t='}' -> acc,q
+    | t::q -> parse_interior_of_an_accolade q (acc^(String.make 1 t));;
 
-let extract_name f = 
-  if not ((Utils.string_starts_with "{" f) || Utils.string_starts_with "*{" f) then "",f else 
-    let rec read_name result lst = 
-      match lst with
-      | [] -> result,[]
-      | t::q when t='}' -> result,q
-      | t::q -> read_name (result^(String.make 1 t)) q
-    in let rec read lst = 
-         match lst with 
-         | [] -> "",""
-         | t::q when t='{' -> 
-           let name,q = read_name "" q in
-           name,(Utils.list_to_string q)
-         | _::q -> read q
-    in read (Utils.string_to_list f);;
+let parse_command list_of_chars =
+  let rec parse_command_rec list_of_chars acc = 
+    match list_of_chars with
+      | [] -> acc,[]
+      | t::q when t='{' -> 
+        let a,q = parse_interior_of_an_accolade q "" in
+        let acc = (match acc with 
+          | NullCommand -> AtomCmd a 
+          | AtomCmd b -> SimpleCmd (b,a) 
+          | SimpleCmd (b,c) -> MultipleCmd (b,a::c::[]) 
+          | MultipleCmd (b,c) -> MultipleCmd(b,a::c))
+        in parse_command_rec q acc
+      | t::q when (t='\n' || t=' ') -> acc,q
+      | t::q -> match acc with
+        | NullCommand -> parse_command_rec q (AtomCmd (String.make 1 t))
+        | AtomCmd b -> parse_command_rec q (AtomCmd (b^(String.make 1 t)))
+        | _ -> acc,(t::q)
+  in let cmd,l = parse_command_rec list_of_chars (NullCommand)
+  in 
+  match cmd with 
+    | MultipleCmd (s,l2) -> MultipleCmd (s,List.rev l2),l
+    | e -> e,l;;
 
-let create_subsubsection i str =
-  let str = String.trim str in 
-  if str="" then Nil else
-    let newline = Str.regexp "\n" in
-    let line_list = Str.split newline str
-    in let name,rest = extract_name (List.hd line_list) in let name,rest = String.trim name,String.trim rest in
-    let new_list = rest::(List.tl line_list)
-    in let tl = List.map (String.trim) new_list
-    in let tl = List.filter (fun a -> not (String.equal "" a)) tl
-    in let str = String.concat "\n" tl
-    in Subsubsection(name,[Node((str),tl)],i);;
-
-let parse_subsubsection str =
-  let subsubsection = Str.regexp "\\\\subsubsection" in
-  let subsubsection_list = Str.split subsubsection str in
-  let subsubsection_list = List.tl subsubsection_list in
-  let a = List.map (fun c -> create_subsubsection 0 c) (subsubsection_list) in
-  shuffle a;;
-
-let create_generic func (cons: string * section list * int -> section) i str = 
-  let str = String.trim str in
-  if str = "" then Nil else 
-    let newline = Str.regexp "\n"
-    in let line_list = Str.split newline str 
-    in let name,rest = extract_name (List.hd line_list) in let name,rest = String.trim name,String.trim rest
-    in let tl = List.tl line_list
-    in let chapter_list = func (String.concat "\n" tl)
-    in let tl = List.map (String.trim) tl
-    in let tl = List.filter (fun a -> not (String.equal "" a)) tl
-    in let str = String.concat "\n" tl
-    in let chapter_list = if chapter_list=[] then [Node(str,[str])] else chapter_list 
-    in let first_node = if rest = "" then Nil else Node(rest,[rest])
-    in let chapter_list = if rest = "" then chapter_list else first_node::chapter_list
-    in if name="" then BigNode(chapter_list) else cons(name,chapter_list,i);;
-
-let parse_generic ?(transform=Fun.id) regexp func str =
-  let re = Str.regexp regexp in
-  let re_list = Str.split re str in
-  let re_list = transform re_list in
-  let a = List.map (fun c -> func 0 c) (re_list)
-  in shuffle a;;
-
-(**Automatic detection of Glossary*)
-let detect_prelude fichier =
-  let str = String.concat "\n" (Utils.read_file fichier) in
-  let first = Str.split (Str.regexp "\\\\begin{document}") str |> List.hd in
-  let tbl = Hashtbl.create 1 in
-  let line_list = Str.split (Str.regexp "\n") first in
-  let rec extract_name result list = 
-    match list with
-    | [] -> result
-    | t::_ when t='}' -> result
-    | t::q -> extract_name (result^(String.make 1 t)) q
-  in let rec detect lst =
-       match lst with 
-       | [] -> tbl
-       | t::q when (Utils.string_starts_with "\\input{" t) ->
-         let n = extract_name "" (Utils.string_to_list t) 
-         in let n = Str.global_replace (Str.regexp "\\\\input{") "" n  
-         in Hashtbl.add tbl "gloss" n; detect q
-       | t::q when (Utils.string_starts_with "\\title{" t) -> 
-         let n = extract_name "" (Utils.string_to_list t) 
-         in let n = Str.global_replace (Str.regexp "\\\\title{") "" n
-         in let n = Str.global_replace (Str.regexp "\\\\\\\\") ":" n 
-         in Hashtbl.add tbl "title" n; detect q
-       | _::q -> detect q
-  in detect line_list;;
+let append_line str q =
+  match (String.trim (str)) with
+    | "" -> q
+    | e -> Line(e)::q
 
 
-
-let create_subsection = create_generic (parse_subsubsection) (fun (a,b,c) -> Subsection(a,b,c))
-let parse_subsection = parse_generic "\\\\subsection" (create_subsection) 
-let create_section = create_generic (parse_subsection) (fun (a,b,c) -> Section(a,b,c))
-let parse_section = parse_generic "\\\\section" (create_section)
-let create_chapter = create_generic (parse_section) (fun (a,b,c) -> Chap(a,b,c))
-let parse_chapter = parse_generic ~transform:List.tl "\\\\chapter" (create_chapter)
+let pre_parse_file file = 
+  let str = Utils.read_file file in
+  let str = String.concat "\n" str in
+  let rec parse current_acc acc lst = 
+    match lst with
+      | [] -> append_line current_acc acc
+      | t::q when t='\\' -> 
+        let cmd,l = parse_command q in 
+        parse "" (Cmd(cmd)::(append_line current_acc acc)) l
+      | t::q -> parse (current_acc^(String.make 1 t)) acc q
+  in let a = parse "" [] (Utils.string_to_list str)
+  in List.rev a;; 
--- lib/utils.ml
+++ lib/utils.ml
@@ -1,57 +1,16 @@
-(**
-   Utilities
-*)
+module Utils = struct
 let string_starts_with prefix str =
   if String.length prefix > String.length str then false else 
     let a = String.sub str 0 (String.length prefix) in String.equal a prefix;;
-
-(** A function reading the inputed file and outputs a string of the lines separated by a line break \n
-    @param filename the file to read *)
+let string_to_list = fun c -> List.of_seq (String.to_seq c);;
 let read_file filename = 
   let lines = ref [] in
   let chan = open_in filename in
   try
     while true; do
-      let a = input_line chan in if not (string_starts_with "//" a) then lines := a :: !lines
+      let a = input_line chan in lines := a :: !lines
     done; !lines
   with End_of_file ->
     close_in chan;
     List.rev !lines ;;
-
-(** Write a string in a file *)
-let write_to_file file str =
-  let f = open_out file in
-  output_string f str;
-  flush f;
-  close_out f;;
-
-
-let transform_list_of_chars_to_string_without_the_newline lst =
-  let rec aux acc l =
-    match l with 
-    | [] -> acc
-    | t::q when t='\n' -> aux acc q (*UNIX*)
-    | t::q when t='\r' -> aux acc q (*WINDOWS*)
-    | t::q -> print_int (Char.code t); print_newline (); aux (acc ^ (String.make 1 t)) q
-  in aux "" lst;;
-
-let string_to_list = fun c -> List.of_seq (String.to_seq c);;
-let list_to_string list = String.of_seq (List.to_seq list)
-
-let detect_file_type file =
-  let fs = try open_in file |> input_line with End_of_file -> (print_endline "not a valid TeX file"; exit 2)
-  in let rec read_line result lst = 
-       match lst with 
-       | [] -> result
-       | t::_ when t='}' -> result
-       | t::q -> read_line (result ^ (String.make 1 t)) q
-  in let rec read lst =
-       match lst with
-       | [] -> failwith "Only books/article are supported"
-       | t::q when t='{' -> read_line "" q
-       | _::q -> read q
-  in let type_read = read (string_to_list fs)
-  in match type_read with 
-  | "book" -> "book"
-  | "article" -> "article"
-  | _ -> print_endline "only articles/books are supported"; exit 2;;
+  end;;
