96d82cf6ee326e06c7837c469f3d070386b6e738
Charlotte Thomas <contact@nwa2coco.fr>
2022-03-19 21:27:19 +0100

    add preamble separation

--- lib/parser.ml
+++ lib/parser.ml
@@ -7,6 +7,9 @@ type structure =
   | Nul
   | Line of string
   | Cmd of cmd 
+  | AtomicCmd of string
+  | OneArgCmd of string * structure list
+  | MultipleArgCmd of string * structure list list
   | Env of string * structure list
   | Subsubsection of string * structure
   | Subsection of string * structure
@@ -70,15 +73,45 @@ let append_line str q =
     | e -> Line(e)::q
 
 
-let pre_parse_file file = 
-  let str = Utils.read_file file in
-  let str = String.concat "\n" str in
-  let rec parse current_acc acc lst = 
-    match lst with
-      | [] -> append_line current_acc acc
-      | t::q when t='\\' -> 
-        let cmd,l = parse_command q in 
-        parse "" (Cmd(cmd)::(append_line current_acc acc)) l
-      | t::q -> parse (current_acc^(String.make 1 t)) acc q
+
+let rec parse_string str = 
+let rec parse current_acc acc lst = 
+      match lst with
+        | [] -> append_line current_acc acc
+        | t::q when t='\\' -> 
+          let cmd,l = parse_command q in 
+          parse "" (Cmd(cmd)::(append_line current_acc acc)) l
+        | t::q -> parse (current_acc^(String.make 1 t)) acc q
+and parse_nested_commands ast_list = 
+  match ast_list with
+    | [] -> []
+    | Line s::q -> (Line s)::(parse_nested_commands q)
+    | Cmd c::q -> 
+      let a = (match c with
+        | NullCommand -> Nul
+        | AtomCmd s -> AtomicCmd s
+        | SimpleCmd (s,s2) -> OneArgCmd (s,(parse_string s2))
+        | MultipleCmd (s,s2) -> MultipleArgCmd (s,(List.map (parse_string) s2)))
+      in a::(parse_nested_commands q)
+    | _::q -> parse_nested_commands q
   in let a = parse "" [] (Utils.string_to_list str)
+  in let a = parse_nested_commands a
   in List.rev a;; 
+
+(*Preamble / Document*)
+
+let separate_preamble lst = 
+  let rec iter ast_list a0 a1 =
+    match ast_list with
+      | (OneArgCmd (s,(Line s1)::_))::q when (String.equal s "begin") && (String.equal s1 "document") -> 
+          iter q a0 (OneArgCmd (s,(Line s1)::[])::a1)
+      | e::q when (List.length a1 = 0) -> iter q (e::a0) a1 
+      | e::q -> iter q a0 (e::a1)
+      | [] -> List.rev a0,List.rev a1 
+  in iter lst [] [];;
+ 
+
+let pre_parse_file file = 
+    let str = Utils.read_file file in
+    let str = String.concat "\n" str in
+    parse_string str   
