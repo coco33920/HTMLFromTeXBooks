a621f5818e5be991b0d2109fca7825ccbfc09efe
Charlotte Thomas <contact@nwa2coco.fr>
2022-03-23 17:21:19 +0100

    add building and downloading shell scripts

---
+++ building.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/coco33920/HTMLFromTeXBooks
+cd HTMLFromTeXBooks
+dune build @install
+dune install 
---
+++ downloading.sh
@@ -0,0 +1,3 @@
+wget https://github.com/coco33920/HTMLFromTeXBooks/releases/latest/download/htmlfromtexbooks
+mv htmlfromtexbooks ~/.local/bin/htmlfromtexbooks
+chmod +x ~/.local/bin/htmlfromtexbooks
